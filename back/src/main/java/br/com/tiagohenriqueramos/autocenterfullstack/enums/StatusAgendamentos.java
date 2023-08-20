e8720725bba2990ecb03ededccd063bbcd16a29d
Tiago Ramos <tiagohenriqueramos@hotmail.com>
2023-08-20 14:44:17 -0300

    deletando pasta br back-end

--- back/src/main/java/br/com/tiagohenriqueramos/autocenterfullstack/enums/StatusAgendamentos.java
+++
@@ -1,7 +0,0 @@
-	package br.com.tiagohenriqueramos.autocenterfullstack.enums;
-
-public enum StatusAgendamentos {
-
-	CONCLUIDO, PENDENTE, CANCELADO
-
-}
--- back/src/main/java/br/com/tiagohenriqueramos/autocenterfullstack/repositories/NotaFiscalRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.tiagohenriqueramos.autocenterfullstack.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.tiagohenriqueramos.autocenterfullstack.entities.NotaFiscal;
-
-public interface NotaFiscalRepository extends JpaRepository<NotaFiscal, Long> {
-
-}
